d6a3569b540bdbfcfa95bcd2a47204e36eb3abe1
Jason Bratt <jasonhero858@gmail.com>
2016-03-20 14:54:55 -0500

    finished styling for leaderboards page

---
+++ src/client/components/statsTable.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+class StatsTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <table className='table table-striped'>
+        <TableHeader headers={Object.keys(this.props.data[0])} />
+        <TableBody bodyData={this.props.data} />
+      </table>
+    )
+  }
+}
+
+const TableHeader = ({headers}) => {
+  let theaders = headers.map((header) => {
+    return (
+      <th>
+        {header}
+      </th>
+    )
+  });
+  return (
+    <thead>
+      <tr>
+        {theaders}
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = ({bodyData}) => {
+  let rows = bodyData.map((row) => {
+    return (
+        <TableRow rowData={row}/>
+    )
+  });
+  return (
+    <tbody>
+      {rows}
+    </tbody>
+  )
+}
+
+const TableRow = ({rowData}) => {
+  let headers = Object.keys(rowData);
+  let row = headers.map((key) => {
+    if (key.toLowerCase() === 'playername') {
+      return (
+        <td>
+          <img className='playerhead-table' src={'https://mcapi.ca/avatar/2d/' + rowData[key] + '/20'}/>
+          <span className='playername-table'>{rowData[key]}</span>
+        </td>
+      )
+    }
+    return (
+      <td>
+        {rowData[key]}
+      </td>
+    )
+  })
+  return (
+    <tr>
+      {row}
+    </tr>
+  )
+}
+
+export default StatsTable;
--- src/client/routes/index/init.js
+++ src/client/routes/index/init.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Griddle from 'griddle-react';
 
+// Load Components
+import StatsTable from '../../components/StatsTable';
+
 
 let fake = [
   {
@@ -60,7 +63,7 @@ let fake = [
                 kills: 403,
                 deaths: 338,
                 headshots: 80
-              },
+              }
 ];
 class Index extends React.Component {
   constructor(props) {
@@ -76,7 +79,7 @@ class Index extends React.Component {
   }
   render() {
     return (
-      <div id='wrapper'>
+      <div className='wrapper'>
         <Sidebar />
         <PageBody />
       </div>
@@ -93,7 +96,7 @@ const Sidebar = ({}) => {
       <div className='sidebar-divider'>
         <div className='divider-wrapper'>
           <div className='divider-header'>
-            Player Search
+            Profile Search
           </div>
           <div className='sidebar-search'>
             <input id='player_search' type='text' className='form-control' placeholder='Playername'/>
@@ -119,8 +122,68 @@ const Sidebar = ({}) => {
 const PageBody = ({}) => {
   return (
     <div id='page-content-wrapper'>
-      <div id='content-overlay'>
-        <Leaderboards />
+      <Profile />
+    </div>
+  )
+}
+
+const Profile = ({}) => {
+  return (
+    <div className='wrapper'>
+      <div id='upper-content'>
+        <div className='content-wrap'>
+            <div className='container-fluid'>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <div className='leaderboard-name'>
+                    Survival Games with Guns
+                  </div>
+                  <div className='leaderboard-image'>
+                    <img src='/images/sgrl.png'/>
+                    <div className='after'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div id='bottom-content'>
+        <div className='content-wrap'>
+          <div className='row'>
+            <div className='leaderboad-aggregation'>
+              <div className='leaderboard-sort'>
+                <label>Sort by Date:</label>
+                <select className='form-control'>
+                  <option>Last Week</option>
+                </select>
+              </div>
+              <div className='leaderboard-sort'>
+                <label>Sort by Stat:</label>
+                <select className='form-control'>
+                  <option>Kills</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='leaderboard-stats'>
+              <StatsTable data={fake}/>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='leaderboard-nav'>
+              <div className='pull-middle leaderboard-total'>
+                Total Players: 27001
+              </div>
+              <div className='pull-left btn btn-primary'>
+                Previous Page
+              </div>
+              <div className='pull-right btn btn-primary'>
+                Next Page
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
@@ -128,7 +191,62 @@ const PageBody = ({}) => {
 
 const Leaderboards = ({}) => {
   return (
-    <div>
+    <div className='wrapper'>
+      <div id='upper-content'>
+        <div className='content-wrap'>
+            <div className='container-fluid'>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <div className='leaderboard-name'>
+                    Survival Games with Guns
+                  </div>
+                  <div className='leaderboard-image'>
+                    <img src='/images/sgrl.png'/>
+                    <div className='after'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div id='bottom-content'>
+        <div className='content-wrap'>
+          <div className='row'>
+            <div className='leaderboad-aggregation'>
+              <div className='leaderboard-sort'>
+                <label>Sort by Date:</label>
+                <select className='form-control'>
+                  <option>Last Week</option>
+                </select>
+              </div>
+              <div className='leaderboard-sort'>
+                <label>Sort by Stat:</label>
+                <select className='form-control'>
+                  <option>Kills</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='leaderboard-stats'>
+              <StatsTable data={fake}/>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='leaderboard-nav'>
+              <div className='pull-middle leaderboard-total'>
+                Total Players: 27001
+              </div>
+              <div className='pull-left btn btn-primary'>
+                Previous Page
+              </div>
+              <div className='pull-right btn btn-primary'>
+                Next Page
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
