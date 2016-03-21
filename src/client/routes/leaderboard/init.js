3e3ef011ac1230696a85f09390e4609248ebd930
Jason Bratt <jasonhero858@gmail.com>
2016-03-20 23:46:19 -0500

    fixed dumb css bug

--- src/client/routes/leaderboard/init.js
+++ src/client/routes/leaderboard/init.js
@@ -67,24 +67,22 @@ let fake = [
 
 const Leaderboard = ({}) => {
   return (
-    <div>
-      <div className='wrapper'>
-        <div id='upper-content'>
-          <div className='content-wrap'>
-              <div className='container-fluid'>
-                <div className='row'>
-                  <div className='col-md-12'>
-                    <div className='leaderboard-name'>
-                      Survival Games with Guns
-                    </div>
-                    <div className='leaderboard-image'>
-                      <img src='/images/sgrl.png'/>
-                      <div className='after'></div>
-                    </div>
+    <div className='wrapper' id='fuckyou'>
+      <div id='upper-content'>
+        <div className='content-wrap'>
+            <div className='container-fluid'>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <div className='leaderboard-name'>
+                    Survival Games with Guns
+                  </div>
+                  <div className='leaderboard-image'>
+                    <img src='/images/sgrl.png'/>
+                    <div className='after'></div>
                   </div>
                 </div>
               </div>
-          </div>
+            </div>
         </div>
       </div>
       <div id='bottom-content'>
